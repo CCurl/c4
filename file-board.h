98813163cb82e7b64533730abcf9357b21ce5c2d
Chris Curl <curlbud-github@yahoo.com>
2022-10-26 16:28:36 -0400

    Update file-board.h

--- file-board.h
+++ file-board.h
@@ -1,18 +1,18 @@
 #ifndef __FILES__
 
 void fileInit() { }
-void fOpen() { DROP2; push(0); }
-void fClose() { DROP1; }
-void fRead() { DROP3; push(0); }
-void fWrite() { DROP3; push(0); }
-void fGetC() { push(0); }
-void fPutC() { DROP2; }
-void fGetS() { DROP3; push(0); }
-void fDelete() { DROP1; }
-void fList() { }
-void fSaveSys() { }
-int fLoadSys() { return 0; }
-void fLoad(const char *fn) { }
+void fOpen()    { DROP2; push(0); }           // (fn md--fh) -- md: 0=READ, else WRITE
+void fClose()   { DROP1; }                    // (fh--)
+void fRead()    { DROP3; push(0); }           // (a sz fh--n)
+void fWrite()   { DROP3; push(0); }           // (a sz fh--n)
+void fGetC()    { DROP1; push(0); push(0); }  // (fh--c n)
+void fPutC()    { DROP2; }                    // (c fh--)
+void fGetS()    { DROP3; push(0); }           // (a sz fh--n)
+void fDelete()  { DROP1; }                    // (fn--)
+void fList()    { }                           // (--)
+void fSaveSys() { }                           // (--)
+int fLoadSys()  { return 0; }                 // (--)
+void fLoad(const char *fn) { }                // (--)
 
 #else
 
