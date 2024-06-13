72dbe095f43c264839d4a9b13969891cc0ed70e6
Chris Curl <curlc@yahoo.com>
2024-06-13 01:32:57 -0400

    cleanup

--- c4.h
+++ c4.h
@@ -50,13 +50,19 @@ typedef union { FLT_T f; cell i; } SE_T;
 typedef struct { ushort xt; byte sz, fl, lx, ln; char nm[32]; } DE_T;
 
 // These are defined by c4.c
-
-extern int strLen(const char *s);
+extern void strCpy(char *d, const char *s);
+extern int  strLen(const char *s);
 extern void printF(const char *fmt, ...);
+extern int  outer(const char *s);
 extern void Init();
 
 // c4.c needs these
 extern cell inputFp, outputFp;
+extern void zType(const char *str);
+extern void emit(const char ch);
+extern void ttyMode(int isRaw);
+extern int  key();
+extern int  qKey();
 extern void fileInit();
 extern void filePush(cell fh);
 extern cell filePop();
@@ -67,11 +73,5 @@ extern cell fileWrite(char *buf, int sz, cell fh);
 extern int  fileGets(char *buf, int sz, cell fh);
 extern void fileLoad(char *name);
 extern void blockLoad(int blk);
-
-extern void zType(const char *str);
-extern void emit(const char ch);
-extern void ttyMode(int isRaw);
-extern int  key();
-extern int  qKey();
 
 #endif //  __C4_H__
