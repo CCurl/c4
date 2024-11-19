de24bb96800cf858e8d7c4ccb70d08988f79a6c0
Chris Curl <ccurl@solventum.com>
2024-11-19 10:19:33 -0500

    Stupid VS copy re-formatting

--- file-pc.c
+++ file-pc.c
@@ -1,22 +1,20 @@
 #include "c4.h"
 
-#ifndef FILE_NONE
-
 // Support for files
 cell inputFp, outputFp, fileStk[FSTK_SZ + 1];
 int fileSp;
 
 void filePush(cell fh) { if (fileSp < FSTK_SZ) { fileStk[++fileSp] = fh; } }
 cell filePop() { return (0 < fileSp) ? fileStk[fileSp--] : 0; }
-static char* blockFn(char* fn, int blk) { sprintf(fn, "block-%03d.fth", blk); return fn; }
+static char *blockFn(char *fn, int blk) { sprintf(fn, "block-%03d.fth", blk); return fn; }
 static char fn[32];
 
 void fileInit() { fileSp = 0; inputFp = 0; }
-cell fileOpen(const char* name, const char* mode) { return (cell)fopen(name, mode); }
+cell fileOpen(const char *name, const char *mode) { return (cell)fopen(name, mode); }
 void fileClose(cell fh) { fclose((FILE*)fh); }
-void fileDelete(const char* name) { remove(name); }
-cell fileRead(char* buf, int sz, cell fh) { return fread(buf, 1, sz, (FILE*)fh); }
-cell fileWrite(char* buf, int sz, cell fh) { return fwrite(buf, 1, sz, (FILE*)fh); }
+void fileDelete(const char *name) { remove(name); }
+cell fileRead(char *buf, int sz, cell fh) { return fread(buf, 1, sz, (FILE*)fh); }
+cell fileWrite(char *buf, int sz, cell fh) { return fwrite(buf, 1, sz, (FILE*)fh); }
 void blockLoad(int blk) { fileLoad(blockFn(fn, blk)); }
 
 void blockLoadNext(int blk) {
@@ -25,18 +23,16 @@ void blockLoadNext(int blk) {
     if (inputFp == 0) { inputFp = filePop(); }
 }
 
-int fileGets(char* buf, int sz, cell fh) {
+int fileGets(char *buf, int sz, cell fh) {
     buf[0] = 0;
     if (fh == 0) { fh = (cell)stdin; }
     if (fgets(buf, sz, (FILE*)fh) != buf) return 0;
     return strLen(buf);
 }
 
-void fileLoad(const char* name) {
+void fileLoad(const char *name) {
     cell fh = fileOpen(name, "rt");
     if (fh == 0) { zTypeF("-[%s] not found-", name); return; }
     if (inputFp) { filePush(inputFp); }
     inputFp = fh;
 }
-
-#endif // FILE_NONE
